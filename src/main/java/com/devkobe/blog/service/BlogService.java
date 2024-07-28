3abbdab11bd5ccfcde5939457e2c0ab2f94d170d
Kobe <dev.skyachieve91@gmail.com>
2024-07-29 05:37:44 +0900

    :sparkles: Feat: 'BlogService 생성 및 구현.'

---
+++ src/main/java/com/devkobe/blog/service/BlogService.java
@@ -0,0 +1,19 @@
+package com.devkobe.blog.service;
+
+import com.devkobe.blog.domain.Article;
+import com.devkobe.blog.dto.AddArticleRequest;
+import com.devkobe.blog.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service // 빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
