c9f718d05127e06cc9b5c914b1622268889a16ac
Kobe <dev.skyachieve91@gmail.com>
2024-07-29 06:16:03 +0900

    :sparkles: Feat: 'findAll 메서드 구현.'

--- src/main/java/com/devkobe/blog/service/BlogService.java
+++ src/main/java/com/devkobe/blog/service/BlogService.java
@@ -3,6 +3,7 @@ package com.devkobe.blog.service;
 import com.devkobe.blog.domain.Article;
 import com.devkobe.blog.dto.AddArticleRequest;
 import com.devkobe.blog.repository.BlogRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,10 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // DB에 저장되어 있는 글을 모두 가져오는 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
