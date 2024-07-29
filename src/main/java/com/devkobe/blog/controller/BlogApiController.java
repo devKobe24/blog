30a9d38153d9771f6208af80950b1efa749c30c3
Kobe <dev.skyachieve91@gmail.com>
2024-07-29 14:36:32 +0900

    :sparkles: Feat: 'deleteArticle 메서드 생성 및 구현.'
    /api/articles/{id} DELETE 요청이 오면 글을 삭제하기 위한 deleteArticle 메서드를 생성 및 구현하였습니다.

--- src/main/java/com/devkobe/blog/controller/BlogApiController.java
+++ src/main/java/com/devkobe/blog/controller/BlogApiController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
             .body(new AriticleResponse(article));
+    }
+
+    // DELETE 요청이 오면 글을 삭제하는 메서드
+    @DeleteMapping("/api/article/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+            .build();
     }
 }
