a721b3bb0a2e965da15e115ca2249f0714cddff2
Kobe <dev.skyachieve91@gmail.com>
2024-07-29 14:58:08 +0900

    :sparkles: Feat: 'update 메서드 생성 및 구현.'

--- src/main/java/com/devkobe/blog/service/BlogService.java
+++ src/main/java/com/devkobe/blog/service/BlogService.java
@@ -2,10 +2,12 @@ package com.devkobe.blog.service;
 
 import com.devkobe.blog.domain.Article;
 import com.devkobe.blog.dto.AddArticleRequest;
+import com.devkobe.blog.dto.UpdateArticleRequest;
 import com.devkobe.blog.repository.BlogRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service // 빈으로 등록
@@ -32,5 +34,16 @@ public class BlogService {
     // 글 삭제 메서드
     public void delete(Long id) {
         blogRepository.deleteById(id);
+    }
+
+
+    @Transactional // 트랜잭션 메서드
+    public Article update(Long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Not Found id =======>>>>>>> " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
