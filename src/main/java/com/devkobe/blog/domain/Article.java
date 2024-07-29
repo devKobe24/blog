268b04b9131a155c71c6ca0cf4b0762024f84623
Kobe <dev.skyachieve91@gmail.com>
2024-07-29 14:51:01 +0900

    :sparkles: Feat: 'update 메서드 생성 및 구현.'
    수정 API 서비스 메서드 코드 작성, 엔티티에 요청받은 내용으로 값을 수정하는 메서드를 작성.

--- src/main/java/com/devkobe/blog/domain/Article.java
+++ src/main/java/com/devkobe/blog/domain/Article.java
@@ -30,4 +30,10 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    // 글 수정 메서드
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
