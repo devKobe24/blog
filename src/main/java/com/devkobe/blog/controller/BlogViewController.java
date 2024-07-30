562920e975600476b97ced2ca7dfdba3cd291991
Kobe <dev.skyachieve91@gmail.com>
2024-07-30 15:18:33 +0900

    :recycle: Refactor: 'newArticle 메서드 추가.'
    수정 화면을 보여주기 위한 컨트롤러 메서드 추가.

--- src/main/java/com/devkobe/blog/controller/BlogViewController.java
+++ src/main/java/com/devkobe/blog/controller/BlogViewController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -36,5 +37,16 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    // 수정 화면을 보여주기 위한 메서드.
+    @GetMapping("/new-article")
+    // 1. id 키를 가진 쿼리 파라미터 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) { // 2. id가 없으면 생성.
+            model.addAttribute("article", new ArticleViewResponse());
+        }
+
+        return "newArticle";
     }
 }
