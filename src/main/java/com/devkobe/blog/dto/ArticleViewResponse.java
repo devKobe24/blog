c71aa48ea440e42cff89bd2e4caccaf22b4e0f21
Kobe <dev.skyachieve91@gmail.com>
2024-07-30 10:50:13 +0900

    :sparkles: Feat: 'ArticleViewResponse DTO 생성 및 구현.'
    뷰에서 사용할 ArticleViewResponse DTO를 생성 및 구현하였습니다.

---
+++ src/main/java/com/devkobe/blog/dto/ArticleViewResponse.java
@@ -0,0 +1,23 @@
+package com.devkobe.blog.dto;
+
+import com.devkobe.blog.domain.Article;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class ArticleViewResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
