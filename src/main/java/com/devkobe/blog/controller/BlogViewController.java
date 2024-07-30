a0a45cf57653df60b7627bc0c9a358cab5e58a3d
Kobe <dev.skyachieve91@gmail.com>
2024-07-30 11:00:31 +0900

    :sparkles: Feat: 'getArticle 메서드 생성 및 구현.'
    메서드의 인자인 id에 URL로 넘어온 값을 받아 findById() 메서드로 넘겨 글을 조회하고, 화면에서 사용할 모델에 데이터를 저장한 다음, 보여줄 화면의 템플릿 이름을 반환하는 코드를 작성하였습니다.

--- src/main/java/com/devkobe/blog/controller/BlogViewController.java
+++ src/main/java/com/devkobe/blog/controller/BlogViewController.java
@@ -1,19 +1,22 @@
 package com.devkobe.blog.controller;
 
+import com.devkobe.blog.domain.Article;
 import com.devkobe.blog.dto.ArticleListViewResponse;
+import com.devkobe.blog.dto.ArticleViewResponse;
 import com.devkobe.blog.service.BlogService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class BlogViewController {
 
     private final BlogService blogService;
-
+    // 블로그 글 전체 리스트를 담은 뷰를 반환하는 메서드, GET
     @GetMapping("/articles")
     public String getArticles(Model model) {
         List<ArticleListViewResponse> articles = blogService.findAll().stream()
@@ -22,5 +25,16 @@ public class BlogViewController {
         model.addAttribute("articles", articles); // 1. 블로그 글 리스트 저장.
 
         return "articleList"; // 2. articleList.html라는 뷰 조회
+    }
+
+    // 블로그 글을 반환할 메서드, GET
+    // 메서드의 인자(argument)인 id에 URL로 넘어온 값을 받아 findById() 메서드로 넘겨
+    // 글을 조회하고, 화면에서 사용할 모델에 데이터를 저장한 다음, 보여줄 템플릿 이름을 반환합니다.
+    @GetMapping("articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
